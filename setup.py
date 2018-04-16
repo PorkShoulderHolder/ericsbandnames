b73e56e5c8d9eabe37f26ef889c431963d2eac3e
porkshoulderholder <sam.royston@breather.com>
2018-04-16 18:47:16 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='ericsbandnames',
       packages=['ericsbandnames'],
-      version='0.2',
+      version='0.3',
       description='Simple client for scraping the contents of ericsbandnames.com, some of which may be offensive, lewd, and/or puerile. However, majority of them are hilarious.',
       author='Sam Fox Royston (code), Eric Andre (content)',
       author_email='sfoxroyston@gmail.com',
