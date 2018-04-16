340912a17e6da3e2663a0ef1b648c832135d3e46
porkshoulderholder <sam.royston@breather.com>
2018-04-16 18:47:02 -0400

    version bump

--- setup.py
+++ setup.py
@@ -1,18 +1,5 @@
 from setuptools import setup
 
-
-
-def parse_requirements(filename):
-    """ load requirements from a pip requirements file """
-    lineiter = (line.strip() for line in open(filename))
-    return [line for line in lineiter if line and not line.startswith("#")]
-
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements("requirements.txt")
-print(install_reqs)
-# reqs = [str(ir.req) for ir in install_reqs]
-
-
 setup(name='ericsbandnames',
       packages=['ericsbandnames'],
       version='0.2',
@@ -22,7 +9,7 @@ setup(name='ericsbandnames',
       license="MIT",
       url='https://github.com/PorkShoulderHolder/ericsbandnames',
       keywords=["text", "joke", "eric andre", "nsfw", "scraping"],
-      install_requires=install_reqs,
+      install_requires=["requests", "beautifulsoup4"],
       entry_points='''
           [console_scripts]
           ericsbandnames=ericsbandnames:cli
