0c8b3743eecc21e90562300f13593e004e84e584
porkshoulderholder <sam.royston@breather.com>
2018-04-16 16:11:56 -0400

    setpu.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from distutils.core import setup, Extension
+import unittest
+
+
+setup(name='ericsbandnames',
+      packages=['ericsbandnames'],
+      version='0.1',
+      description='Simple client for scraping the contents of ericsbandnames.com, some of which may be offensive, lewd, and/or puerile. However, majority of them are hilarious.',
+      author='Sam Fox Royston (code), Eric Andre (content)',
+      author_email='sfoxroyston@gmail.com',
+      license="MIT",
+      url='https://github.com/PorkShoulderHolder/ericsbandnames',
+      keywords=["text", "joke", "eric andre", "nsfw", "scraping"],
+      )
+
